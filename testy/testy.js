6ca8c1b6c5c2370438303996c3752cb7d7f5b59e
emka-zet <noemi.zachoszcz@gmail.com>
2020-02-02 16:55:38 +0100

    add uuu

---
+++ testy/testy.js
@@ -0,0 +1,9 @@
+document.getElementById("testy").innerHTML = "Moj pierwszy skrypt w js";
+let a = "Hej ty";
+let b = " bałwanie";
+console.log(a + b);
+document.getElementById("testy").innerHTML = a + b;
+function mojaFunkcja() {
+    document.getElementById("testy").innerHTML = " O co ci chodzi ty bałwanie?";
+    console.log(onclick);
+}
